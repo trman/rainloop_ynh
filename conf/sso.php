9bc9fb67c3988b724f5d3b25083d4b768b1050e7
scith <scith@hotmail.fr>
2015-10-07 22:02:12 +0200

    Fix

--- conf/sso.php
+++ conf/sso.php
@@ -2,7 +2,7 @@
 
 // Enable RainLoop Api and include index file 
 $_ENV['RAINLOOP_INCLUDE_AS_API'] = true;
-include '/var/www/rainloop/index.php';
+include '/var/wwwPATHTOCHANGE/index.php';
 
 // Retrieve email and password
 if (isset($_SERVER['HTTP_EMAIL']) && isset($_SERVER['PHP_AUTH_PW'])) {
@@ -11,8 +11,8 @@ if (isset($_SERVER['HTTP_EMAIL']) && isset($_SERVER['PHP_AUTH_PW'])) {
 	$ssoHash = \RainLoop\Api::GetUserSsoHash($email, $password);
 
 	// redirect to webmail sso url
-	\header('Location: https://domain.tld/rainloop/index.php?sso&hash='.$ssoHash);
+	\header('Location: https://domain.tldPATHTOCHANGE/index.php?sso&hash='.$ssoHash);
 }
 else {
-	\header('Location: https://domain.tld/rainloop/index.php');
+	\header('Location: https://domain.tldPATHTOCHANGE/index.php');
 }
