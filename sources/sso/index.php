a7266f08d98332f6f7f1885974da115af796d07b
Djip007 <djip.perois@free.fr>
2015-10-21 20:23:50 +0200

    - mise a jour de l'install - ajout nouveau sso - install et activation du plugins ynh

---
+++ sources/sso/index.php
@@ -0,0 +1,32 @@
+<?php
+
+if (!defined('APP_VERSION')) {
+	define('APP_VERSION', '1.9.3.363');
+	define('APP_INDEX_ROOT_FILE', __FILE__);
+	define('APP_INDEX_ROOT_PATH', str_replace('\\', '/', rtrim(dirname(__FILE__), '\\/').'/'));
+}
+
+if(isset($_GET["auto_log"])) {
+	$_ENV['RAINLOOP_INCLUDE_AS_API'] = true;
+}
+
+if (file_exists(APP_INDEX_ROOT_PATH.'rainloop/v/'.APP_VERSION.'/include.php')) {
+	include APP_INDEX_ROOT_PATH.'rainloop/v/'.APP_VERSION.'/include.php';
+} else {
+	echo '[105] Missing version directory';
+	exit(105);
+}
+
+if(isset($_GET["auto_log"])) {
+	if (isset($_SERVER['HTTP_EMAIL']) && isset($_SERVER['PHP_AUTH_PW'])) {
+		$email = $_SERVER['HTTP_EMAIL'];
+		$password = $_SERVER['PHP_AUTH_PW'];
+		$ssoHash = \RainLoop\Api::GetUserSsoHash($email, $password);
+
+		// redirect to webmail sso url
+		\header('Location: https://domain.tldPATHTOCHANGE/index.php?sso&hash='.$ssoHash);
+	}
+	exit();
+}
+
+?>
