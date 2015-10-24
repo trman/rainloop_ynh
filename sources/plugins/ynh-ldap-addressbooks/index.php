b114d8cc4586404c5f2e0cbfdbac86bdc9a45b59
scith <scith@hotmail.fr>
2015-10-24 14:24:50 +0200

    LDAP suggestions
    Thanks to Djip007

---
+++ sources/plugins/ynh-ldap-addressbooks/index.php
@@ -0,0 +1,37 @@
+<?php
+class YnhLdapAddressbooksPlugin extends \RainLoop\Plugins\AbstractPlugin {
+	public function Init() {
+		$this->addHook ( 'main.fabrica', 'MainFabrica' );
+	}
+	
+	/**
+	 * test if ldap is supported (but with ynh it nead ;) )
+	 *
+	 * @return string
+	 */
+	public function Supported() {
+		if (! \function_exists ( 'ldap_connect' )) {
+			return 'The LDAP PHP exention must be installed to use this plugin';
+		}
+		return '';
+	}
+	
+	/**
+	 *
+	 * @param string $sName        	
+	 * @param mixed $mResult        	
+	 */
+	public function MainFabrica($sName, &$mResult) {
+		if ($sName === 'suggestions') {
+			include_once __DIR__ . '/YnhLdapAddressbooks.php';
+			
+			if (! \is_array ( $mResult )) {
+				$mResult = array ();
+			}
+			
+			$oProvider = new YnhLdapAddressbooks ();
+			$mResult [] = $oProvider;
+		}
+	}
+}
+?>
